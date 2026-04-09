63cb343167975a24b4e21529abd394deb8ea2716
ytjeong <102654500+sighingpotato@users.noreply.github.com>
2026-04-09 09:55:52 +0900

    Delete Desktop/workspace/codekata directory

--- Desktop/workspace/codekata/src/no1/Solution.java
+++
@@ -1,9 +0,0 @@
-package no1;
-
-public class Solution {
-    public int solution(int num1, int num2) {
-        // 0 -> num1 - num2로 수정
-        int answer = num1 - num2;
-        return answer;
-    }
-}
