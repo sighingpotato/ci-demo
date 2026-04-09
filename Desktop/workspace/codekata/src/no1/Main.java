42b1eeca76c631c5c26885933ca0d31b0987a1c0
sighingpotato <jot0202@naver.com>
2026-04-09 09:49:11 +0900

    update

--- Desktop/workspace/codekata/src/no1/Main.java
+++ Desktop/workspace/codekata/src/no1/Main.java
@@ -5,11 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
 
+        // 예시
         int num1 = 2;
         int num2 = 3;
 
         int result = sol.solution(num1, num2);
 
+        // 결과 출력
         System.out.println("결과: " + result);
     }
 }
