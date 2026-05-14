2719749657c8fb2d0dc31953d0ab83b343c5f13f
베려하는마음 <102654500+sighingpotato@users.noreply.github.com>
2026-05-14 16:34:54 +0900

    Delete Desktop/workspace/advance directory

--- Desktop/workspace/advance/src/main/java/com/example/advance/domain/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.advance.domain.comment.repository;
-
-import com.example.advance.common.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
